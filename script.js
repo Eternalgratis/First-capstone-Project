253b2104346a32e18078d6fb86f3ce2fdad183be
eternalgratis <tyaps002@gmail.com>
2022-07-02 03:12:52 +0100

    Add about

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const projectContainerArr = [
   {
     headline: "Featured Speakers",
-    image: "../images/minister.jfif",
+    image: "../images/ministersssss.jfif",
     name: "Eternalgratis",
     Text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
     Subtext:
@@ -53,48 +53,56 @@ for (let i = 0; i < projectContainerArr.length; i += 1) {
   featureSection.innerHTML = `
   <h3>${projectContainerArr[0].headline}</h3>
   <div class="mobile-speakers">
-    <div class="ms-list1 view-more1">
-      <img width="120" src="${projectContainerArr[0].image}" alt="picture">
+  <div class="speak-dir">
+    <div class="view-more">
+      <img src="${projectContainerArr[0].image}" alt="picture">
       <div class="cite-quote"> 
       <cite id="anchor">${projectContainerArr[0].name}</cite>
-      <blockquote><span id="subtext">${projectContainerArr[0].Text}</span>${projectContainerArr[0].Subtext}</blockquote>
+      <blockquote><span id="subtext">${projectContainerArr[0].Text}</span><br>${projectContainerArr[0].Subtext}</blockquote>
       </div>
     </div>
-    <div class="ms-list2 view-more2">
-    <img width="120" src="${projectContainerArr[1].image}" alt="picture">
+    <div class="view-more">
+    <img src="${projectContainerArr[1].image}" alt="picture">
     <div class="cite-quote">
     <cite id="anchor">${projectContainerArr[1].name}</cite>
-    <blockquote><span id="subtext">${projectContainerArr[1].Text}</span>${projectContainerArr[1].Subtext}</blockquote>
+    <blockquote><span id="subtext">${projectContainerArr[1].Text}</span><br>${projectContainerArr[1].Subtext}</blockquote>
     </div>
     </div>
-    <div class="view-more" id="view-more3">
-    <img width="120" src="${projectContainerArr[2].image}" alt="picture">
+    <div class="view-more view">
+    <img src="${projectContainerArr[2].image}" alt="picture">
     <div class="cite-quote">
     <cite id="anchor">${projectContainerArr[2].name}</cite>
-    <blockquote><span id="subtext">${projectContainerArr[2].Text}</span>${projectContainerArr[2].Subtext}</blockquote>
+    <blockquote><span id="subtext">${projectContainerArr[2].Text}</span<br>${projectContainerArr[2].Subtext}</blockquote>
     </div>
     </div>
-    <div class="view-more" id="view-more4">
-    <img width="120" src="${projectContainerArr[3].image}" alt="picture">
+  </div>
+
+  <div class="speak-dir">
+    <div class="view-more view">
+    <img src="${projectContainerArr[3].image}" alt="picture">
     <div class="cite-quote">
     <cite id="anchor">${projectContainerArr[3].name}</cite>
-    <blockquote><span id="subtext">${projectContainerArr[3].Text}</span>${projectContainerArr[3].Subtext}</blockquote>
+    <blockquote><span id="subtext">${projectContainerArr[3].Text}</span><br>${projectContainerArr[3].Subtext}</blockquote>
     </div>
     </div>
-    <div class="view-more" id="view-more5">
-    <img width="120" src="${projectContainerArr[4].image}" alt="picture">
+
+    <div class="view-more view">
+    <img src="${projectContainerArr[4].image}" alt="picture">
     <div class="cite-quote">
     <cite id="anchor">${projectContainerArr[4].name}</cite>
-    <blockquote><span id="subtext">${projectContainerArr[4].Text}</span>${projectContainerArr[4].Subtext}</blockquote>
+    <blockquote><span id="subtext">${projectContainerArr[4].Text}</span><br>${projectContainerArr[4].Subtext}</blockquote>
     </div>
     </div>
-    <div class="view-more" id="view-more6">
-    <img width="120" src="${projectContainerArr[5].image}" alt="picture">
+
+    <div class="view-more view">
+    <img src="${projectContainerArr[5].image}" alt="picture">
     <div class="cite-quote">
     <cite id="anchor">${projectContainerArr[5].name}</cite>
-    <blockquote><span id="subtext">${projectContainerArr[5].Text}</span>${projectContainerArr[5].Subtext}</blockquote>
+    <blockquote><span id="subtext">${projectContainerArr[5].Text}</span><br>${projectContainerArr[5].Subtext}</blockquote>
     </div>
     </div>
+  </div>
+  <button id="mobile-link1">MORE</button>
   </div>`;
 }
 // const capStoneObj = [
